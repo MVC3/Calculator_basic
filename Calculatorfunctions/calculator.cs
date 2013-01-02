0ba11737c6ec62ca3c4825601b66687b358e5906
Usha <ushachinnu333@gmail.com>
2013-01-02 12:01:21 -0600

    Initial Upload

---
+++ Calculatorfunctions/calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculatorfunctions
+{
+    public class calculator
+    {
+        public int calculate(int a, int b, string Operator)
+        {
+            
+            if (c == OperatorType.Add)
+            { 
+            }
+            return ;
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+
+
+    
+    }
+
+    public enum OperatorType
+    { 
+    Add= "+",
+    Substract = "-",
+    Multiply = "3",
+    Divide = "4",
+    }
+}
